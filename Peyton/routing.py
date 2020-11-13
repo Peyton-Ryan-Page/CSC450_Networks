75534bee4cc738f60207afc6cef3b9922138cd93
Peyton-Ryan-Page <62871196+Peyton-Ryan-Page@users.noreply.github.com>
2020-11-13 17:31:48 -0600

    Update routing.py

--- Peyton/routing.py
+++ Peyton/routing.py
@@ -3,6 +3,9 @@
 import csv
 from sys import argv
 
+import copy
+
+
 TOPOLOGY_FILE = argv[1]
 
 with open(TOPOLOGY_FILE, newline='') as topology:
@@ -12,10 +15,14 @@ with open(TOPOLOGY_FILE, newline='') as topology:
 	rows = []
 	for row in fileReader:
 		rows.append(row)
-		for (key, value) in row.items():
-			# print(key, value)
-			if key == '':
+		for (key, value) in list(row.items()):
+        	# print(key, value)
+			if key == "":
 				nodes.append(value)
+			elif value == '9999':
+				del row[key]
+			else:
+				row[key] = int(row[key])
 				
 	i = 0
 
@@ -43,16 +50,35 @@ if(source_node not in nodes):
 	exit()
 print(f"Shortest path tree for node {source_node}:")
 
-			
-untested = connections
-distances = {source_node:connections[source_node][source_node]}
-untested = nodes
-untested.remove(source_node)
+N_prime = [source_node]
+untested = copy.deepcopy(connections)
+del untested[source_node][source_node]
 
-print(untested)
+distances = {}
 
-print(f"CONNECTIONS: {connections[source_node]}")
-# N_prime = {source_node}
 
-# for node in untested:
-# 	if()
+# for key, entry in connections.items():
+# 	print(key, entry)
+
+for key, entry in connections[source_node].items():
+	distances[key] = entry
+
+while(N_prime != nodes):
+	minimum = 9999
+	for key, value in untested[source_node].items():
+		if key in N_prime:
+			pass
+		elif value < minimum:
+			minimum = value
+			minimum_node = key
+
+	N_prime.append(minimum_node)
+	del untested[source_node][minimum_node]
+	N_prime.sort()
+
+	distances[node] = min(distances[node], distances[minimum_node] + connections[minimum_node][node])
+
+
+		
+
+print(distances)
